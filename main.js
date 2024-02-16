e178fad232a1ed8e296e7b375b5a229c8ee6fb79
David-MacD <david-p-macdonald@outlook.com>
2024-02-16 11:19:44 +0000

    Implement rules table and update the code

--- main.js
+++ main.js
@@ -2,17 +2,42 @@
 // 1 = Paper
 // 2 = Scissors
 
-const choiceStringArray = ["Rock", "Paper", "Scissors"];
+const rules = [
+	{ text: "Rock", emoji: "🤘", winsAgainst: [1, 3] },
+	{ text: "Lizard", emoji: "🦎", winsAgainst: [2, 4] },
+	{ text: "Spock", emoji: "🖖", winsAgainst: [3, 0] },
+	{ text: "Scissors", emoji: "✂", winsAgainst: [4, 1] },
+	{ text: "Paper", emoji: "🧻", winsAgainst: [0, 2] },
+];
+
+/*
+ * Returns
+ *   Tie: 0
+ *   choice1 Wins: 1
+ *   choice1 Loses: -1
+ */
+function resolveWin(choice1, choice2) {
+	if (choice1 === choice2) {
+		return 0;
+	}
+	const result = rules[choice1].winsAgainst.includes(Number(choice2));
+	console.log(result);
+	return result ? 1 : -1;
+}
 
 function choiceToString(choice) {
-	return choiceStringArray[choice];
+	return rules[choice].text;
+}
+
+function choiceToEmoji(choice) {
+	return rules[choice].emoji;
 }
 
 function getUserChoice() {
+	const choices = rules.map((rule, i) => `${i} = ${choiceToEmoji(i)}`);
+
 	const question = `Choose a number!
-  0 = Rock
-  1 = Paper
-  2 = Scissors
+${choices.join("\n")}
   `;
 
 	let choice = prompt(question);
@@ -45,7 +70,7 @@ ${result}`);
 
 function playGame(score) {
 	// Get choices
-	const computerChoice = Math.floor(Math.random() * 3).toString();
+	const computerChoice = Math.floor(Math.random() * rules.length).toString();
 	const userChoice = getUserChoice();
 
 	// Convert the choice numbers to strings
@@ -56,46 +81,34 @@ function playGame(score) {
 	console.log("Computer Choice:", computerChoiceString);
 
 	const showGameResult = configureShowGameResult(
-		userChoiceString,
-		computerChoiceString,
+		choiceToEmoji(userChoice),
+		choiceToEmoji(computerChoice),
 	);
 
 	if (userChoice === null) {
 		showMessage("You've ended the game!");
-	}
+	} else {
+		const result = resolveWin(userChoice, computerChoice);
 
-	// Tie
-	if (userChoice === computerChoice) {
-		showGameResult("It was a tie!");
-	}
-	// Win
-	else if (userChoiceString === "Rock") {
-		if (computerChoiceString === "Paper") {
-			showGameResult("Computer wins");
-			score.computer += 1;
-		} else if (computerChoiceString === "Scissors") {
+		// Tie
+		if (result === 0) {
+			showGameResult("It was a tie!");
+		}
+		// User wins
+		else if (result === 1) {
 			showGameResult("User wins");
 			score.user += 1;
 		}
-	} else if (userChoiceString === "Paper") {
-		if (computerChoiceString === "Scissors") {
+		// Computer Wins
+		else if (result === -1) {
 			showGameResult("Computer wins");
 			score.computer += 1;
-		} else if (computerChoiceString === "Rock") {
-			showGameResult("User wins");
-			score.user += 1;
-		}
-	} else if (userChoiceString === "Scissors") {
-		if (computerChoiceString === "Rock") {
-			showGameResult("Computer wins");
-			score.computer += 1;
-		} else if (computerChoiceString === "Paper") {
-			showGameResult("User wins");
-			score.user += 1;
 		}
 	}
 	return { ...score };
 }
+
+// const useEmojis = prompt("Use Emojis? Y / N").toUpperCase();
 
 let score = playGame({ user: 0, computer: 0 });
 
